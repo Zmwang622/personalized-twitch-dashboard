21d6babee29dc59990f5131656d369e56074822e
Ming <36709118+Zmwang622@users.noreply.github.com>
2021-08-14 06:54:50 +0000

    Add barebone html + server structure

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -1,4 +1,12 @@
 // This is where all the logic for your Twitch API will live!
 export default async (req, res) => {
-  res.status(200).json({data: 'Welcome to Twitch API!'})
+  try {
+    if (req.method === 'POST') {
+      const { data } = req.body
+      res.status(200).json({ data })
+    }
+  } catch (error) {
+    console.warn(error.message)
+    res.status(500).send()
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,53 @@
 // Main entry point of your app
+import React, { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
+  // State
+  const [favoriteChannels, setFavoriteChannels] = useState([])
+
+  // Actions
+  const addStreamChannel = async event => {
+    event.preventDefault()
+
+    const { value } = event.target.elements.name
+
+    if (value) {
+      console.log(`value: ${value}`)
+
+      // Call Twitch APIt
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch(`${path}/api/twitch`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data: value })
+
+      })
+
+      const json = await response.json()
+
+      console.log(`From the server: ${json.data}`)
+
+      setFavoriteChannels(prevState => [...prevState, value])
+
+      event.target.elements.name.value = ""
+    }
+  }
+
+  // Render Method
+  const renderForm = () => (
+    <div className={styles.formContainer}>
+      <form onSubmit={addStreamChannel}>
+        <input id="name" placeholder="Twitch Channel Name" type="text" required />
+        <button type="submit">Add Streamer</button>
+      </form>
+    </div>
+  )
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +55,8 @@ const Home = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={styles.inputContainer}>
-        <h1>Welcome to the Personalized Twitch Dashboard! 🎉</h1>
+        {renderForm()}
+        <div>{favoriteChannels.join(",")}</div>
       </div>
     </div>
   )
